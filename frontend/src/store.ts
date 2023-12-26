3c4526ce0556d45d32ca6b2ca0541ff51ceb079f
chedi lahmer <131680831+ChediLahmer@users.noreply.github.com>
2023-12-26 19:30:52 +0100

    Update store.ts

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,13 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import buttonReducer from "./features/buttonSlice";
-//import modalReducer from "./features/modalSlice";
-//import editReducer from "./features/editSlice";
 import combinedReducer from "./features/combinedSlice";
 const store = configureStore({
   reducer: {
     button: buttonReducer,
-   // modal: modalReducer,
-   // edit: editReducer,
     combined: combinedReducer,
   },
 });
